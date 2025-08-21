698fbff0bcc2a1aaef082d336579260b97e691c3
unknown <bergamoclara9@gmail.com>
2025-08-21 10:23:37 -0300

    feat: desafio 1

---
+++ Desafio1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite um texto.");
+var texto = Console.ReadLine();
+
+static string replaceVogal(string texto)
+{
+    return texto.Replace('A', '@').Replace('a', '@')
+                .Replace('E', '#').Replace('e', '#')
+                .Replace('I', '!').Replace('i', '!')
+                .Replace('O', '$').Replace('o', '$')
+                .Replace('U', '%').Replace('u', '%');
+}
+
+Console.WriteLine("Resultado final: " + replaceVogal(texto));
